ce3454a460fdfc76b9309d7b98028e61943f9164
Pooja Vyas <pvyas13@my.centennialcollege.ca>
2025-11-27 13:43:48 -0500

    updated backend proxy in Backend json + removed my previous change in api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api", // proxy to backend
+    baseURL: "/api", // proxy to backend
 });
 
 api.interceptors.request.use((config) => {
