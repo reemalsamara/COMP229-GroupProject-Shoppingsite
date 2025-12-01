88f395df136a8eabb70f2f019bb40e6f7200009c
Reem Alsamara <reemalsamara@gmail.com>
2025-11-30 19:30:06 -0500

    Fix axios baseURL for Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "/api", // proxy to backend
+    baseURL: "https://shopstack-backend-pzxk.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
